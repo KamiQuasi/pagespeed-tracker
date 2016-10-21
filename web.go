589aaec5158b9b285dcd278e7c0ada1b10e8431e
Luke Dary <ldary@redhat.com>
2016-10-20 22:00:30 -0400

    Initial result mapping

--- web.go
+++ web.go
@@ -1,14 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
 
+// PageSpeed struct
+type PageSpeed struct {
+	Kind         string    `json:"numberResources"`
+	ID           string    `json:"numberResources"`
+	ResponseCode int       `json:"numberResources"`
+	Title        string    `json:"numberResources"`
+	RuleGroups   RuleGroup `json:"ruleGroups"`
+	PageStats    PageStats `json:"pageStats"`
+	Version      Version   `json:"Version"`
+}
+
+// RuleGroup struct
+type RuleGroup struct {
+	Speed     Rule `json:"SPEED"`
+	Usability Rule `json:"USABILITY"`
+}
+
+// Rule struct
+type Rule struct {
+	Score int `json:"score"`
+}
+
+// PageStats struct
+type PageStats struct {
+	NumberResources         int    `json:"numberResources"`
+	NumberHosts             int    `json:"numberHosts"`
+	TotalRequestBytes       string `json:"totalRequestBytes"`
+	NumberStaticResources   int    `json:"numberStaticResources"`
+	HTMLResponseBytes       string `json:"htmlResponseBytes"`
+	CSSResponseBytes        string `json:"cssResponseBytes"`
+	ImageResponseBytes      string `json:"imageResponseBytes"`
+	JavaScriptResponseBytes string `json:"javascriptResponseBytes"`
+	OtherResponseBytes      string `json:"otherResponseBytes"`
+	NumberJsResources       int    `json:"numberJsResources"`
+	NumberCSSResorces       int    `json:"numberCssResources"`
+}
+
+// Version struct
+type Version struct {
+	Major int `json:"major"`
+	Minor int `json:"minor"`
+}
+
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", home)
 	bind := fmt.Sprintf("%s:%s", os.Getenv("OPENSHIFT_GO_IP"), os.Getenv("OPENSHIFT_GO_PORT"))
 	fmt.Printf("listening on %s...", bind)
 	err := http.ListenAndServe(bind, nil)
@@ -17,7 +61,7 @@ func main() {
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
+func home(res http.ResponseWriter, req *http.Request) {
 	resp, err := http.Get("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=http://developers.redhat.com")
 	if err != nil {
 
@@ -27,5 +71,8 @@ func hello(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 
 	}
-	fmt.Fprintf(res, "%s", body)
+	result := PageSpeed{}
+	json.Unmarshal(body, &result)
+	enc := json.NewEncoder(os.Stdout)
+	fmt.Fprintf(res, "%s", enc.Encode(result))
 }
